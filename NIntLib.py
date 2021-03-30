a07a671a86a8bdba41d26ed091f0b19bc18c5d39
shb101 <shobhit.bhatnagar.96@gmail.com>
2021-03-30 22:04:34 +0530

    using ctypes now for interfacing python and c

--- NIntLib.py
+++ NIntLib.py
@@ -1,22 +1,36 @@
-from subprocess import Popen, PIPE
+import ctypes
 import math
+import time
 
 def NIntTrapz(func, x0, x1, N):
-    p = Popen(['NIntEngine.exe'], shell=True, stdout=PIPE, stdin=PIPE)
-    # Input N, x0, x1:
-    in1 = bytes('0 ' + str(N) + ' ' + str(x0) + ' ' + str(x1) + '\n', 'UTF-8')
-    p.stdin.write(in1)
-    p.stdin.flush()
-    # Input func values:
+    engine = ctypes.cdll.LoadLibrary('NIntegrationEngine.dll')
     dx = (x1-x0)/N
+    F = [func(x0+i*dx) for i in range(N+1)]
+    ctypes_F = (ctypes.c_double * (N+1))(*F)
+    engine.NIntTrapz.restype = ctypes.c_double
+    I = engine.NIntTrapz(ctypes.c_double(x0), ctypes.c_double(x1), ctypes_F, ctypes.c_int(N))
+    return I
+
+def PyNIntTrapz(func, x0, x1, N):
+    dx = (x1 - x0)/N
+    I = 0
     for i in range(N+1):
-        in2 = bytes(str(func(x0 + i*dx)) + '\n', 'UTF-8')
-        p.stdin.write(in2)
-        p.stdin.flush()
-    # Read the result:
-    result = p.stdout.readline().strip()
-    return result
+        I += dx * (func(x0+dx*i) + func(x0+dx*(i+1)))/2
+    return I
 
 # Testing
-func = lambda x: math.sin(math.pi*x/2)
-print(NIntTrapz(func,0,1,10000))
+#func = lambda x: math.sin(math.pi*x/2)
+func = lambda x: math.exp(-x**2/2) * math.sin(math.sqrt(x))
+N = 1000000
+
+print('1. C++')
+t1 = time.time()
+print(NIntTrapz(func,0,1,N))
+t2 = time.time()
+print('Time taken = ' + str(t2-t1) + ' seconds')
+
+print('2. Py')
+t1 = time.time()
+print(PyNIntTrapz(func,0,1,N))
+t2 = time.time()
+print('Time taken = ' + str(t2-t1) + ' seconds')
